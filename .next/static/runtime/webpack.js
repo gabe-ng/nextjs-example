ed2f8c2e182e1f6205584391f56403899869364d
Gabriel Ng <gabrielng15@gmail.com>
2018-10-30 14:11:17 -0700

    Added layout component

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b859993f67d6300b90eb";
+/******/ 	var hotCurrentHash = "561082acf744114d88e6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
